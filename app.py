b4c2f3e757de27d525acd82b74ffb1a64566f7e4
Renansserpa <61283507+Renansserpa@users.noreply.github.com>
2020-07-21 19:58:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("Plotly examples")
 st.sidebar.title('Aqui temos um título provavelmente maneiro')
+url= 'coord-ita.csv'
+@st.cache(persist=True)
+def load_data():
+  data= pd.read_csv(url)
+  return data
+data= load_data()   
