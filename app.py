353fed7df57d4c03c703897e5be14005c43bdf42
Renansserpa <61283507+Renansserpa@users.noreply.github.com>
2020-07-22 11:57:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,11 @@ st.write(data)
 st.sidebar.subheader("Isso  um subheader")
 random_tweet = st.sidebar.radio('Sentiment',('Fazenda Penedo','Jambeiro','Maringá'))
 st.sidebar.markdown(data.query('Bairro==@random_tweet')['Bairro'].sample(n=1).values[0])
+select=st.sidebar.selectbox('tipo de visualizaÃÂ§ÃÂ£o',('Histogram','Pie chart'),key='1')
+if not st.sidebar.checkbox('Hide',True):
+    st.markdown('ta acontecendo')
+    if select == 'Histogram':
+        fig= px.bar({'Sentiment':['positive','neutral','negative'],'Tweets':[
+        20,30,40
+        ]},x='Sentiment',y='Tweets',color='Tweets',height=500)
+        st.plotly_chart(fig)
